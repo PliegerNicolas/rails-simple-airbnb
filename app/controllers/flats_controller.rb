38aec8292c89d62d7eb186394eaa76655d4324d4
Nicolas Plieger <plieger05@gmail.com>
2020-02-08 10:34:31 +0000

    Not finished

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,9 +4,13 @@ class FlatsController < ApplicationController
   end
 
   def create
+    flat = Flat.new(flat_params)
+    flat.save
+    redirect_to flats_path
   end
 
   def new
+    @flat = Flat.new
   end
 
   def edit
@@ -16,8 +20,33 @@ class FlatsController < ApplicationController
   end
 
   def update
+    @flat.update(flat_params)
+    redirect_to flats_path
   end
 
   def destroy
+    set_flat
+    Flat.destroy(@flat.id)
+    redirect_to flats_path
+  end
+
+  def search
+    # ongoing work
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(
+      :name,
+      :address,
+      :description,
+      :price_per_night,
+      :number_of_guests
+    )
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,57 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do
+1.times do
   Flat.create!(
     name: Faker::BossaNova.song,
     address: Faker::Address.full_address,
     description: Faker::Lorem.words(number: 24),
     price_per_night: Faker::Number.between(from: 60, to: 800),
-    number_of_guests: Faker::Number.between(from: 1, to: 8)
+    number_of_guests: Faker::Number.between(from: 1, to: 8),
+    image: 'https://images.unsplash.com/photo-1579142595966-c062d938de43?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max'
+  )
+end
+
+1.times do
+  Flat.create!(
+    name: Faker::BossaNova.song,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.words(number: 24),
+    price_per_night: Faker::Number.between(from: 60, to: 800),
+    number_of_guests: Faker::Number.between(from: 1, to: 8),
+    image: 'https://images.unsplash.com/photo-1580223237741-b259b7caa468?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max'
+  )
+end
+
+1.times do
+  Flat.create!(
+    name: Faker::BossaNova.song,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.words(number: 24),
+    price_per_night: Faker::Number.between(from: 60, to: 800),
+    number_of_guests: Faker::Number.between(from: 1, to: 8),
+    image: 'https://images.unsplash.com/photo-1579713420544-776f6fd95ffb?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max'
+  )
+end
+
+1.times do
+  Flat.create!(
+    name: Faker::BossaNova.song,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.words(number: 24),
+    price_per_night: Faker::Number.between(from: 60, to: 800),
+    number_of_guests: Faker::Number.between(from: 1, to: 8),
+    image: 'https://images.unsplash.com/photo-1580700499118-117855dc4f99?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max'
+  )
+end
+
+1.times do
+  Flat.create!(
+    name: Faker::BossaNova.song,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.words(number: 24),
+    price_per_night: Faker::Number.between(from: 60, to: 800),
+    number_of_guests: Faker::Number.between(from: 1, to: 8),
+    image: 'https://images.unsplash.com/photo-1580220278238-9c55a934149a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max'
   )
 end
